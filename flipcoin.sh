0e85610c73d93b726a8acf8fe4829bba10c1b71b
“saad297” <skamil60@gmail.com>
2020-05-27 20:21:50 +0530

    Added : Win Or Loose At 21

--- flipcoin.sh
+++ flipcoin.sh
@@ -3,7 +3,7 @@ echo "Welcome to the Head and Tails program."
 HEADCOUNT=0
 TAILCOUNT=0
 
-for (( i=1;i<=50;i++ ))
+for (( i=1;i<=100;i++ ))
 do
 
 	x=$(($RANDOM%2))
@@ -11,11 +11,24 @@ do
 	if [ $x -eq 0 ]
 	then
 		((HEADCOUNT++))
+		if [ $HEADCOUNT -eq 21 ]
+		then
+			y=$(($HEADCOUNT -$TAILCOUNT))
+			echo "Heads won by $y"
+			break
+		fi
+		
 	else
 		((TAILCOUNT++))
+		 if [ $TAILCOUNT -eq 21 ]
+                then
+                        z=$(($TAILCOUNT - $HEADCOUNT))
+			echo "Tails won by $z"
+			break
+                fi
+
 	fi
 done
 
-echo "Heads appeared $HEADCOUNT times"
-
-echo "Tails appeared $TAILCOUNT times"
+echo "Heads : $HEADCOUNT"
+echo "Tails : $TAILCOUNT"
